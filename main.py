174d258e101ebbb63e62639912a6b0ad22056661
Madeleine Eastwood <madeleine@Madeleines-MacBook-Pro.local>
2024-04-11 13:06:59 -0400

    encode function

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-# print("hello")
-# print("hi")
-
+#mme lab 9
 def encode(pw):
-    print("hello")
+    #pw will be an 8-digit string of integers
+    encoded_pw = ''
+    for dig in pw:
+        dig = int(dig)
+        dig += 3
+        dig = str(dig)
+        encoded_pw += dig
+    return encoded_pw
+
 
 
